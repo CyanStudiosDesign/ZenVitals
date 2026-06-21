07b0dbb625db9e62b309ec312c9873875e550cf5
Cyan <codinginstant@example.com>
2026-06-22 01:13:17 +0530

    Removed supabase from config

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -38,22 +38,5 @@ export default defineConfig({
         ],
       },
     },
-    // 🚀 NESTED FILE MODULE ADDED HERE
-    file: {
-      resolve: "@medusajs/medusa/file",
-      options: {
-        providers: [
-          {
-            resolve: "medusav2-file-supabase-storage/modules/file",
-            id: "supabase",
-            options: {
-              supabaseUrl: process.env.SUPABASE_URL,
-              apiKey: process.env.SUPABASE_KEY,
-              bucketName: process.env.SUPABASE_BUCKET,
-            },
-          },
-        ],
-      },
-    },
   },
 });
